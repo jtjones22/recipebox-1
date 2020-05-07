af382cf1e5a5472f6957b56e90ca7d3be44b35d9
Janelle Kuhns <j3ckuhns@gmail.com>
2020-05-06 18:15:18 -0700

    add gitignore file

--- recipes/urls.py
+++ recipes/urls.py
@@ -2,9 +2,9 @@ from django.urls import path
 from recipes import views
 
 urlpatterns = [
-    path('', views.index),
-    path('authors/', views.author_detail),
-    path('recipes/', views.recipe_detail)
+    path('', views.index, name='Homepage'),
+    path('author/<int:author_id>/', views.author_detail),
+    path('recipe/<int:recipe_id>/', views.recipe_detail),
 
     #path('admin/', admin.site.urls),
 ]
--- recipes/views.py
+++ recipes/views.py
@@ -5,13 +5,23 @@ from recipes.models import Author
 
 # Create your views here.
 def index(request):
-    data = Recipe.objects.all()
-    return render(request, 'index.html', {'data': data})
+    recipes = Recipe.objects.all()
+    return render(request, 'index.html', {'recipes': recipes})
+
+def author(request, author_id):
+    author = Author.objects.get(id=author_id)
+    recipes = Recipe.objects.filter(author==author)
+    return render(request, 'author_detail.html', {
+        'author': author,
+        'recipes': recipes
+    })
+
 
 def author_detail(request):
     authors = Author.objects.all()
     return render(request, 'author_detail.html', {'authors': authors})
 
-def recipe_detail(request):
-    recipes = Recipe.objects.all()
+
+def recipe_detail(request, recipe_id):
+    recipes = Recipe.objects.get(id=recipe_id)
     return render(request, 'recipe_detail.html', {'recipes': recipes})
